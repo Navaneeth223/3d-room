0c646bad07f6ae09fee12980185e24479914cb95
Navaneeth223 <navaneethkv1002@gmail.com>
2025-06-02 11:49:21 +0530

    first commit

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/4D ROOM SCENE.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/first_pandulam0001-6285.mov",
+    },
+];
